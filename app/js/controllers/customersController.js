a91749e949978ff86eeac3067177b2a5f7ce14af
kartik <rkartik1992@gmail.com>
2016-03-27 00:41:59 +0530

    Add factory.

--- app/js/controllers/customersController.js
+++ app/js/controllers/customersController.js
@@ -1,15 +1,15 @@
 'use strict';
 (function() {
-    var CustomersController = function($scope) {
+    var CustomersController = function($scope, customersFactory) {
         $scope.sortBy = 'name';
         $scope.reverse = false;
 
-        $scope.customers = [
-            { 'id': 1, 'joined': '2000-12-02', 'name': "Kartik", 'city': "Nanded", 'orderTotal': 1, 'orders': [{ 'id': 1, 'product': 'shoes', 'total': 9.9 }] },
-            { 'id': 2, 'joined': '2001-12-02', 'name': "Sam", 'city': "Kinwat", 'orderTotal': 3, 'orders': [{ 'id': 1, 'product': 'shoes', 'total': 9.9 }] },
-            { 'id': 3, 'joined': '2002-12-02', 'name': "Rohan", 'city': "Palghar", 'orderTotal': 11, 'orders': [{ 'id': 1, 'product': 'shoes', 'total': 9.9 }] },
-            { 'id': 4, 'joined': '2003-12-02', 'name': "Hitesh", 'city': "Palghar", 'orderTotal': 12, 'orders': [{ 'id': 1, 'product': 'shoes', 'total': 9.9 }] }
-        ];
+        $scope.customers = [];
+
+        function init(){
+            $scope.customers = customersFactory.getCustomers();
+        }
+        init();
 
         $scope.doSort = function(propName) {
             $scope.sortBy = propName;
@@ -18,7 +18,7 @@
 
     };
 
-    CustomersController.$inject = ['$scope'];
+    CustomersController.$inject = ['$scope', 'customersFactory'];
 
     angular.module('customersApp')
         .controller('CustomersController', CustomersController);
---
+++ app/js/services/customersFactory.js
@@ -0,0 +1,27 @@
+(function() {
+    'use strict';
+    var customersFactory = function() {
+        var customers = [
+            { 'id': 1, 'joined': '2000-12-02', 'name': "Kartik", 'city': "Nanded", 'orderTotal': 1, 'orders': [{ 'id': 1, 'product': 'shoes1', 'total': 9.9 }, { 'id': 12, 'product': 'temp', 'total': 9.9 }] },
+            { 'id': 2, 'joined': '2001-12-02', 'name': "Sam", 'city': "Kinwat", 'orderTotal': 3, 'orders': [{ 'id': 1, 'product': 'shoes2', 'total': 9.9 }] },
+            { 'id': 3, 'joined': '2002-12-02', 'name': "Rohan", 'city': "Palghar", 'orderTotal': 11, 'orders': [{ 'id': 1, 'product': 'shoes3', 'total': 9.9 }, { 'id': 1, 'product': 'banana', 'total': 19.9 }] },
+            { 'id': 4, 'joined': '2003-12-02', 'name': "Hitesh", 'city': "Palghar", 'orderTotal': 12, 'orders': [{ 'id': 1, 'product': 'shoes4', 'total': 9.9 }] }
+        ];
+        var factory = {};
+        factory.getCustomers = function() {
+            return customers;
+        };
+        factory.getCustomer = function(id) {
+        	for (var i = 0, len = customers.length; i < len; i++) {
+                if (customers[i].id === id) {
+                    return customers[i];
+                }
+            }
+            return {};
+        };
+        return factory;
+    };
+
+    angular.module('customersApp')
+        .factory('customersFactory', customersFactory);
+}());
